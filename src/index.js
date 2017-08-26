13c7c1cbee4d03f19feb434f265365739a5b6041
Nic Schumann <nicolas_schumann@brown.edu>
2017-08-26 11:34:46 -0400

    [Style] minor changes to scss, added standard reset.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './style.scss';
 
 import * as d3 from 'd3';
 
-var transform = function( x ) { return x; };
-
 import config from '../package.json';
 import  { arena }  from './arena-api/index.js';
 import { nth_neighborhood } from './components/nth-neighborhood/index.js';
+import { channel_search } from './components/channel-search/index.js';
 
 var api = arena( config );
 
-nth_neighborhood( api, transform, d3, config );
+channel_search( api, d3, config );
